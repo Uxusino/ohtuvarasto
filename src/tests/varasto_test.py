dd1dad4ee6294fe1ad9281091463e9ac13164218
Uxusino Uxusone <89293277+Uxusino@users.noreply.github.com>
2024-11-03 14:16:35 +0200

    Tehtävä 8. Unittest

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,37 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_saldo_luonti(self):
+        self.varasto = Varasto(-10)
+
+        self.assertAlmostEqual(self.varasto.tilavuus, 0.0)
+
+    def test_virheellinen_alku_saldo(self):
+        self.varasto = Varasto(10, -10)
+        self.assertEqual(self.varasto.saldo, 0.0)
+
+    def test_liikaa_alku_saldoa(self):
+        self.varasto = Varasto(10, 100)
+        self.assertEqual(self.varasto.saldo, 10)
+
+    def test_lisaa_virheellinen_ei_muuta_mitaan(self):
+        self.varasto.lisaa_varastoon(-10)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertEqual(self.varasto.saldo, 10)
+
+    def test_ota_virheellinen(self):
+        maara = self.varasto.ota_varastosta(-10)
+        self.assertEqual(maara, 0.0)
+
+    def test_ota_liikaa(self):
+        self.varasto.lisaa_varastoon(10)
+        maara = self.varasto.ota_varastosta(100)
+        self.assertEqual(maara, 10)
+
+    def test_str_oikein(self):
+        teksti = str(self.varasto)
+        self.assertEqual(teksti, "saldo = 0, vielä tilaa 10")
